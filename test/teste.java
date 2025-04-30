7f1b601f11aff48c3cc56d8868a99736acc31494
Julia Strapassan <juliastrapa@gmail.com>
2025-04-30 17:32:48 -0300

    teste

--- test/teste.java
+++ test/teste.java
@@ -1,17 +1,15 @@
 package test;
 
 import org.junit.Test;
-
-import java.util.Scanner;
-
 import static org.junit.Assert.assertEquals;
 
 public class teste {
+
     @Test
     public void testContarCaracteresNome() {
         String nome = "Julia";
         int esperado = 5;
-        int resultado = nome.length();  // comportamento do método
+        int resultado = nome.length();
 
         assertEquals(esperado, resultado);
     }
@@ -21,9 +19,8 @@ public class teste {
         String nome = "Julia";
         String esperado = "ailuJ";
 
-        String resultado = new StringBuilder(nome).reverse().toString();  // mesmo processo do método
+        String resultado = new StringBuilder(nome).reverse().toString();
 
         assertEquals(esperado, resultado);
     }
-
 }
